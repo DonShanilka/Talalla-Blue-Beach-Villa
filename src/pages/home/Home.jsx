9cc270ad711d881e84d6f63c3afe09772fcdeafa
Shanilka <shanilka799@gmail.com>
2024-09-16 12:08:22 +0530

    bluebeachvilla image add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <>
             <div className="home-page-main-rect">
                 <div>
-                    <img src="../public/homePage-image/Jungle-Beach-Bay-Drone-Shot.jpg" alt="" style={{ position: "absolute", width: "100%", height: "100%", top: "0%", left: "0%" }} />
+                    <img src="" alt="" />
                 </div>
                 <div id="main-black-rect"></div>
             </div>
