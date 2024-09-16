9e658cdf5ca717c27f98da1444233deaf748bc2c
Shanilka <shanilka799@gmail.com>
2024-09-16 13:49:45 +0530

    update home page css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <>
             <div className="home-page-main-rect">
-                <div>
+                <div className='image-section'>
                     <img className='homePage-main-image' src="src\assets\HomePageImage\bluebeachvilla.JPG" alt="" />
                 </div>
                 <div id="main-white-rect"></div>
