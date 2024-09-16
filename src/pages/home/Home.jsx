3260f59c980a515c9a096c39167c15fdce111e60
Shanilka <shanilka799@gmail.com>
2024-09-16 11:02:15 +0530

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import '../home/Home.css'
 
 
 const Home = () => {
     return (
         <>
-            <div>
-                <img src="../public/homePage-image/sigiriya.jpg" alt="" style={{ position: "absolute", width: "100%", height: "100%", top: "0%", left: "0%" }} />
+            <div className="home-page-main-rect">
+                <div>
+                    <img src="../public/homePage-image/Jungle-Beach-Bay-Drone-Shot.jpg" alt="" style={{ position: "absolute", width: "100%", height: "100%", top: "0%", left: "0%" }} />
+                </div>
+                <div id="main-black-rect"></div>
             </div>
         </>
     )
