688289a7c66bb773ae6287801d6d19c4f78dff70
Shanilka <shanilka799@gmail.com>
2024-09-16 16:29:22 +0530

    update home page white div css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,7 +8,9 @@ const Home = () => {
             <div className="home-page-main-rect">
                 <div className='image-section'>
                     <img className='homePage-main-image' src="src\assets\HomePageImage\bluebeachvilla.JPG" alt="" />
+
                 </div>
+                <div className='homePage-white-div'></div>
                 <div className='details-section'>
                     <h1>Welcome to the<br /> Talalla Blue Beach Villa </h1>
                 </div>
