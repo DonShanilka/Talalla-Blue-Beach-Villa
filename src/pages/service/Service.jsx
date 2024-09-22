7a34c246ba74325a034f477db25ae13de8896976
Shanilka <shanilka799@gmail.com>
2024-09-22 21:45:23 +0530

    95% don service page

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -10,6 +10,12 @@ function Service() {
                         <img src="src\assets\servicepageImage\IMG_7957.JPG" alt="" className='service-main-image' />
                     </div>
                     <div className='service-details-div'>
+                        <h2 className='service-page-h2'>Our Amenities</h2>
+                        <p className='service-page-para'>
+                            Our hotel offers a range of premium services, including 24/7 concierge,
+                            room service, spa treatments, fitness center, business facilities, and complimentary Wi-Fi,
+                            ensuring a comfortable and seamless stay.
+                        </p>
                         <div className='details-01'>
                             <h5>Front Desk/Reception</h5>
                             <h5>Housekeeping</h5>
