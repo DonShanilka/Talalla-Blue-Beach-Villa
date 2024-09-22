84dab4d0a8010802469ae744890d072ad56d03d8
Shanilka <shanilka799@gmail.com>
2024-09-22 19:58:54 +0530

    update service page details and css

--- src/pages/service/Service.jsx
+++ src/pages/service/Service.jsx
@@ -10,7 +10,22 @@ function Service() {
                         <img src="src\assets\servicepageImage\IMG_7957.JPG" alt="" className='service-main-image' />
                     </div>
                     <div className='service-details-div'>
-
+                        <div className='details-01'>
+                            <h5>Front Desk/Reception</h5>
+                            <h5>Housekeeping</h5>
+                            <h5>Room Service</h5>
+                            <h5>Food and Beverage</h5>
+                            <h5>Concierge Services</h5>
+                            <h5>Valet Parking</h5>
+                        </div>
+                        <div className='details-02'>
+                            <h5>Laundry and Dry Cleaning</h5>
+                            <h5>Meeting Facilities</h5>
+                            <h5>Security</h5>
+                            <h5>Leisure and Recreation</h5>
+                            <h5>Wi-Fi and Technology</h5>
+                            <h5>Transportation Services</h5>
+                        </div>
                     </div>
                 </div>
             </section>
