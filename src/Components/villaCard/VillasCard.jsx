07a1cd189e0c5f571c5bc756d6254df40f2f6216
Shanilka <shanilka799@gmail.com>
2024-09-23 20:55:39 +0530

    update card css

--- src/Components/villaCard/VillasCard.jsx
+++ src/Components/villaCard/VillasCard.jsx
@@ -1,60 +1,33 @@
 import React from 'react'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import '../villaCard/VillasCard.css';
 
-function Villascard() {
-
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
+function Villascard(props) {
 
     return (
         <>
             <div>Villascard</div>
-            <div className='card' style={{
-                position: "absolute", zIndex: 5,
-                top: "20%", left: "-5%", width: "100%",
-                color: "black", overlay: "none"
-            }}>
-                <Carousel responsive={responsive}>
+            <div className='card'>
+                <div>
+                    <img src={props.url} alt="villas" />
+                </div>
+                <div className='card-details-div'>
+                    <h2>{props.name}</h2>
                     <div>
-                        <img src="src\assets\villasImage\villa-zendaya-3.jpg" alt="product" width={300} height={180} />
-                        <h2>Villa Zendaya</h2>
-                        <div className='card-01-details-div'>
-                            <div>
-                                <p>Double Bedrooms x 2</p>
-                            </div>
-                            <div>
-                                <p>Bathrooms x 2</p>
-                            </div>
-                            <div>
-                                <p> Free WiFi,  Minibar,</p>
-                            </div>
-                            <div>
-                                <p>Air conditioning,  Bathtub</p>
-                            </div>
-                        </div>
+                        <p>{props.dis1}</p>
                     </div>
-                    <div>Item 2</div>
-                    <div>Item 3</div>
-                    <div>Item 4</div>
-                </Carousel>
+                    <div>
+                        <p>{props.dis2}</p>
+                    </div>
+                    <div>
+                        <p>{props.dis3}</p>
+                    </div>
+                    <div>
+                        <p>{props.dis4}</p>
+                    </div>
+                    <button>Explore villa</button>
+                </div>
             </div>
         </>
     )
--- src/pages/booking/Bookingpage.jsx
+++ src/pages/booking/Bookingpage.jsx
@@ -2,12 +2,90 @@ import React from 'react'
 import '../booking/Booking.css'
 import { Card } from '@mui/material'
 import Villascard from '../../Components/villaCard/Villascard.jsx'
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function Bookingpage() {
+
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    const productData = [
+        {
+            id: 1,
+            imageUrl: 'https://talallablue.com/wp-content/uploads/2022/05/villa-zendaya-3-1024x576.jpg',
+            alt: "villas",
+            price: "$50",
+            dis1: "Double Bedrooms x 2",
+            dis2: "Bathrooms x 2",
+            dis3: "Free WiFi,  Minibar,",
+            dis4: "Air conditioning,  Bathtub"
+        },
+        {
+            id: 2,
+            imageUrl: "src\assets\villasImage\villa-zendaya-3.jpg",
+            alt: "villas",
+            price: "$50",
+            dis1: "Double Bedrooms x 2",
+            dis2: "Bathrooms x 2",
+            dis3: "Free WiFi,  Minibar,",
+            dis4: "Air conditioning,  Bathtub"
+        },
+        {
+            id: 3,
+            imageUrl: "src\assets\villasImage\villa-zendaya-3.jpg",
+            alt: "villas",
+            price: "$50",
+            dis1: "Double Bedrooms x 2",
+            dis2: "Bathrooms x 2",
+            dis3: "Free WiFi,  Minibar,",
+            dis4: "Air conditioning,  Bathtub"
+        },
+        {
+            id: 4,
+            imageUrl: "src\assets\villasImage\villa-zendaya-3.jpg",
+            alt: "villas",
+            price: "$50",
+            dis1: "Double Bedrooms x 2",
+            dis2: "Bathrooms x 2",
+            dis3: "Free WiFi,  Minibar,",
+            dis4: "Air conditioning,  Bathtub"
+        },
+        {
+            id: 5,
+            imageUrl: "src\assets\villasImage\villa-zendaya-3.jpg",
+            alt: "villas",
+            price: "$50",
+            dis1: "Double Bedrooms x 2",
+            dis2: "Bathrooms x 2",
+            dis3: "Free WiFi,  Minibar,",
+            dis4: "Air conditioning,  Bathtub"
+        }
+    ]
+
+    const product = productData.map((item) => <Villascard name={item.alt} url={item.imageUrl} price={item.price} dis1={item.dis1} dis2={item.dis2} dis3={item.dis3} dis4={item.dis4} />);
+
     return (
         <section>
             <div className='booking-page-main-div'>
-                <Villascard />
+                <Carousel responsive={responsive}>{product}</Carousel>
             </div>
         </section>
     )
