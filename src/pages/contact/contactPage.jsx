e2a9df346f5ebe542e677e48e127f2f585adcc90
Shanilka <shanilka799@gmail.com>
2024-10-15 18:39:12 +0530

    add contact page forms and button

--- src/pages/contact/contactPage.jsx
+++ src/pages/contact/contactPage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import '../contact/contact.css'
 
 function ContactPage() {
     return (
         <>
             <section>
                 <div className='contact-page-main-div'>
-
+                    <div>
+                        <div>
+                            <iframe src="" frameborder="0"></iframe>
+                        </div>
+                        <div className='form-div'>
+                            <div className='form-div-2'>
+                                <input className='input-1' type="text" />
+                                <input className='input-2' type="text" />
+                                <input className='input-3' type="text" />
+                                <input className='input-4' type="text" />
+                                <input className='input-5' type="text" />
+                                <button className='submit-btn'>Submit</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
